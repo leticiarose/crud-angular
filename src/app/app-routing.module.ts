1b45d9b2dba2d5ee2a4285627eb940981aedad18
Letícia Rose <leticia.rosedesantana@gmail.com>
2021-04-04 16:44:04 -0300

    feat: updating titles✨

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,31 @@
-import { ProductUpdateComponent } from './components/products/product-update/product-update.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './views/home/home.component'
-import {ProductCrudComponent} from './views/product-crud/product-crud.component'
-import { ProductCreateComponent } from './components/products/product-create/product-create.component';
 import { ProductDeleteComponent } from './components/products/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/products/product-update/product-update.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { HomeComponent } from "./views/home/home.component";
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { ProductCreateComponent } from './components/products/product-create/product-create.component';
 
 const routes: Routes = [
   {
-  path:"",
-  component: HomeComponent
+    path: "",
+    component: HomeComponent
   },
   {
-  path:"products",
-  component: ProductCrudComponent
+    path: "products",
+    component: ProductCrudComponent
   },
   {
-  path:"products/create",
-  component: ProductCreateComponent
+    path: "products/create",
+    component: ProductCreateComponent
   },
   {
-    path:"products/update/:id",
+    path: "products/update/:id",
     component: ProductUpdateComponent
   },
   {
-    path:"products/delete/:id",
+    path: "products/delete/:id",
     component: ProductDeleteComponent
   },
 ];
@@ -34,4 +34,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
