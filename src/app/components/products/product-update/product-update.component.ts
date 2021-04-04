ab6d1189ab3d7888e2195f438ab6dbb3f6f42cc9
Letícia Rose <leticia.rosedesantana@gmail.com>
2021-04-04 02:14:40 -0300

    feat: add page Product Update✨

---
+++ src/app/components/products/product-update/product-update.component.ts
@@ -0,0 +1,29 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.css']
+})
+export class ProductUpdateComponent implements OnInit {
+  product!: Product;
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id!).subscribe(product => {
+      this.product = product
+    });
+  }
+
+  updateProduct(): void{}
+
+  cancel (): void{
+    this.router.navigate(['/products'])
+  }
+
+}
